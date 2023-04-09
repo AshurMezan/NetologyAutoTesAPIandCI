fbd90836b0021f65d81d66767d421b807cf71df4
Lion 2517 <lion2517@yahoo.com>
2023-04-09 12:40:13 +0500

    Ломаю сборку

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -14,6 +14,7 @@ class MobileBankApiTestV1 {
       // Выполняемые действия
       .when()
           .get("/demo/accounts")
+              
       // Проверки
       .then()
           .statusCode(200);
